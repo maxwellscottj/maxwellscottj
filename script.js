2905a393d83e82b2658503960bb9d83da2551697
Scott Maxwell <maxwellscottj@gmail.com>
2022-09-30 15:59:18 -0700

    added support for JSON blog posting with embedded images

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+function generatePost (posts) {
+	wrapper.innerHTML='';
+	for (let i = 0; i < posts.length; i++) {
+		var parsedPost = posts[i];
+		console.log(parsedPost)
+		var newSection = document.createElement('section');
+		var newHeader = document.createElement('h2');
+		var newPara = document.createElement('p');
+		newHeader.innerHTML=`${parsedPost.Title}`;
+		newPara.innerHTML=`${parsedPost.TextBody}`
+		newSection.appendChild(newHeader);
+		newSection.appendChild(newPara);
+		if (parsedPost.Images){
+			for (let i = 0; i < parsedPost.Images.length; i++) {
+				var newImage = document.createElement('img');
+				newImage.src = parsedPost.Images[i].url
+				newSection.appendChild(newImage);
+			}
+		}
+		wrapper.appendChild(newSection);
+	}
+}
+
 function openDropDown() {
+	closeContact()
 	navdropdown.classList.add('visible')
 	nav.classList.remove('minimized')
 	frameWrapper()
@@ -12,6 +36,18 @@ function closeDropDown() {
 function toggleDropDown() {
 	navdropdown.classList.toggle('visible')
 	nav.classList.toggle('minimized')
+}
+
+function openContact() {
+	closeDropDown();
+	contactpopup.classList.add('visible')
+	nav.classList.remove('minimized')
+	frameBio();
+}
+
+function closeContact(){
+	contactpopup.classList.remove('visible')
+	nav.classList.add('minimized')
 }
 
 function frameWrapper() {
