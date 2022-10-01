f97ed05e51bd15c9c4d5eed25ae9472bfef00e38
Scott Maxwell <maxwellscottj@gmail.com>
2022-09-30 18:59:33 -0700

    added support for embedded youtube, image and video captions, banner change on section mouseover

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function generatePost (posts) {
-	wrapper.innerHTML='';
+var postCategory;
+
+function clearPosts() {
+	for (let i = 0; i < wrapper.children.length; i++) {
+		if (!wrapper.children[i].classList.contains('banner')) {
+				wrapper.removeChild(wrapper.children[i])
+		}
+	}
+}
+
+function generatePost(posts) {
+	postCategory = posts;
+	clearPosts()
 	for (let i = 0; i < posts.length; i++) {
 		var parsedPost = posts[i];
 		console.log(parsedPost)
@@ -13,11 +24,59 @@ function generatePost (posts) {
 		if (parsedPost.Images){
 			for (let i = 0; i < parsedPost.Images.length; i++) {
 				var newImage = document.createElement('img');
+				newImage.classList.add('galleryItem')
 				newImage.src = parsedPost.Images[i].url
+				newImage.alt = parsedPost.Images[i].alt
 				newSection.appendChild(newImage);
+				if (parsedPost.Images[i].caption) {
+					var newCaption = document.createElement('p');
+					newCaption.innerHTML = parsedPost.Images[i].caption;
+					newSection.appendChild(newCaption);
+				}
+			}
+		}
+		if (parsedPost.Videos){
+			for (let i = 0; i < parsedPost.Videos.length; i++) {
+				var newVideo = document.createElement('iframe');
+				newVideo.src = parsedPost.Videos[i].url
+				newImage.alt = parsedPost.Images[i].alt
+				newSection.appendChild(newVideo);
+				if (parsedPost.Videos[i].caption) {
+					var newCaption = document.createElement('p');
+					newCaption.innerHTML = parsedPost.Videos[i].caption;
+					newSection.appendChild(newCaption);
+				}
 			}
 		}
 		wrapper.appendChild(newSection);
+	}
+	
+}
+
+function bannerSwap(newBanner) {
+	var bannerURL;
+	switch (newBanner) {
+		case 'Code':
+			bannerURL = 'images/CodeBanner.png'
+			break;
+		case '2D':
+			bannerURL = 'images/2DBanner.png'
+			break;
+		case '3D':
+			bannerURL = 'images/3DBanner.png'
+			break;
+	}
+	mainBanner.parentNode.removeChild(mainBanner);
+	var newBanner = document.createElement('img');
+	newBanner.id = 'mainBanner'
+	newBanner.classList.add('banner')
+	newBanner.src = bannerURL;
+	wrapper.appendChild(newBanner);
+	mainBanner = newBanner;
+	console.log(mainBanner.src)
+	if (postCategory) {
+		clearPosts();
+		generatePost(postCategory);
 	}
 }
 
@@ -60,4 +119,5 @@ function frameBio() {
 	bio.classList.remove('minimized')
 	wrapper.classList.add('minimized')
 	main.classList.remove('wrapperFrame')
+	clearPosts();
 }
